d88a3e6e1a3c085640b3f740fb285a3b92a7cc26
HOTSAUCE234232323 <pckapp26@gmail.com>
2022-12-05 20:51:01 -0500

    Commit Latest

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+import { create } from "domain";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
 
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyAgtTRkf1smT1GQ4g-CfZ43TSTZV15MAic",
@@ -9,3 +11,16 @@ const firebaseApp = initializeApp({
   appId: "1:294364177449:web:c49f7ca554f59d9cf62838",
   measurementId: "G-T7GVZ0XP5G"
 });
+document.getElementById("SignUp").onclick = function(){
+    const auth = getAuth();
+    var email = document.getElementById("Email").value;
+    var password = document.getElementById("Password").value;
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+}
